6895024c33c2f41568e2161d0f40ce42cd5ad89c
Rcoufourier <remi.coufourier@hetic.net>
2022-02-01 17:34:55 +0100

    feat: wip crud docker-compose

--- cmd/httpserver/main.go
+++ cmd/httpserver/main.go
@@ -2,16 +2,22 @@ package main
 
 import (
 	"fmt"
+	"github.com/docker-generator/api/internal/core/services/dockerComposeService"
+	"github.com/docker-generator/api/internal/handlers"
+	"github.com/docker-generator/api/internal/repositories"
 	"github.com/go-chi/chi/v5"
 	"net/http"
 )
 
 
 func main() {
+	dockerComposeRepositoryInstanciated := repositories.NewDockerComposeFirestore()
+	dockerComposeServiceInstanciated := dockerComposeService.New(dockerComposeRepositoryInstanciated)
+	dockerComposeHandler := handlers.NewDockerComposeHTTPHandler(dockerComposeServiceInstanciated)
+
+
 	r := chi.NewRouter()
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("welcome"))
-	})
+	r.Post("/docker-compose/save", dockerComposeHandler.SaveDockerCompose)
 	err := http.ListenAndServe(":8080", r)
 	fmt.Println(err)
 }
