e3b15e86171852cf359d88407f65096b8e03db65
Steven <steven.nativel@orange.fr>
2022-03-07 20:59:19 +0100

    fix: structure redis

--- internal/repositories/dockerHubRepository/dockerHubRepository.go
+++ internal/repositories/dockerHubRepository/dockerHubRepository.go
@@ -1,8 +1,10 @@
 package dockerHubRepository
 
 import (
+	"context"
 	"fmt"
 	domain "github.com/docker-generator/api/internal/core/domain/dockerHubDomain"
+	"github.com/docker-generator/api/internal/repositories/redisRepository"
 	"github.com/m7shapan/njson"
 	"io/ioutil"
 	"log"
@@ -16,11 +18,14 @@ func New() *dockerHubApi {
 }
 
 func (repo *dockerHubApi) Read(image string, tag string) (domain.DockerHubResult, error) {
+	ctx := context.Background()
+
+	rdb, err := redisRepository.GetClient(ctx)
+
 	resp, err := http.Get("https://hub.docker.com/v2/repositories/library/" + image + "/tags/?name=" + tag)
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	jsonDataFromHttp, err := ioutil.ReadAll(resp.Body)
 
 	var dockerHubImage domain.DockerHubImage
@@ -41,12 +46,21 @@ func (repo *dockerHubApi) Read(image string, tag string) (domain.DockerHubResult
 		dockerHubTags = append(dockerHubTags, finalData)
 	}
 
+	//data := dockerHubTags
+	//data := []string{"a", "b", "c", "d", "e"}
+	//for _, v := range data {
+	//	rdb.RPush(ctx, image+"-"+tag, []byte(v))
+	//}
+
+	test := rdb.LRange(ctx, image+"-"+tag, 0, -1)
+	fmt.Println(test)
+
 	dockerHubResult := domain.DockerHubResult{
 		Name: image,
 		Tags: dockerHubTags,
 	}
 
-	fmt.Print(dockerHubResult)
+	//fmt.Print(dockerHubResult)
 
 	return dockerHubResult, nil
 }
---
+++ internal/repositories/redisRepository/getRedisClient.go
@@ -0,0 +1,18 @@
+package redisRepository
+
+import (
+	"context"
+	"github.com/go-redis/redis/v8"
+)
+
+func GetClient(ctx context.Context) (*redis.Client, error) {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379", // use default Addr
+		Password: "",               // no password set
+		DB:       0,                // use default DB
+	})
+
+	_, err := rdb.Ping(ctx).Result()
+
+	return rdb, err
+}
