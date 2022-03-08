6b0b01bb51045f6105c29e29d6918e73bf2038ff
Steven <steven.nativel@orange.fr>
2022-03-08 11:45:07 +0100

    feat: return same data if redis or docker-hub

--- internal/repositories/dockerHubRepository/dockerHubRepository.go
+++ internal/repositories/dockerHubRepository/dockerHubRepository.go
@@ -2,6 +2,7 @@ package dockerHubRepository
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	domain "github.com/docker-generator/api/internal/core/domain/dockerHubDomain"
 	"github.com/docker-generator/api/internal/repositories/redisRepository"
@@ -20,48 +21,55 @@ func New() *dockerHubApi {
 func (repo *dockerHubApi) Read(image string, tag string) (domain.DockerHubResult, error) {
 	ctx := context.Background()
 
-	rdb, err := redisRepository.GetClient(ctx)
+	rdb, _ := redisRepository.GetClient(ctx)
+	length := rdb.LLen(ctx, image+"-"+tag).Val()
 
-	resp, err := http.Get("https://hub.docker.com/v2/repositories/library/" + image + "/tags/?name=" + tag)
-	if err != nil {
-		log.Fatal(err)
-	}
-	jsonDataFromHttp, err := ioutil.ReadAll(resp.Body)
+	var dockerHubTags []string
 
-	var dockerHubImage domain.DockerHubImage
+	if length > 0 {
+		fmt.Println("------------- REDIS -----------------")
+		fmt.Println(rdb.LRange(ctx, image+"-"+tag, 0, -1).Val())
 
-	err = njson.Unmarshal(jsonDataFromHttp, &dockerHubImage)
-	if err != nil {
-		fmt.Print(err)
-	}
+		dockerHubTags = rdb.LRange(ctx, image+"-"+tag, 0, -1).Val()
+	} else {
+		fmt.Println("-------------- DOCKER HUB ----------------")
+		resp, err := http.Get("https://hub.docker.com/v2/repositories/library/" + image + "/tags/?name=" + tag)
 
-	var dockerHubTags []domain.DockerHubTags
-
-	for _, data := range dockerHubImage.Results {
-		var finalData domain.DockerHubTags
-		errormessage := njson.Unmarshal([]byte(string(data)), &finalData)
-		if errormessage != nil {
-			fmt.Println(errormessage)
+		if err != nil {
+			log.Fatal(err)
 		}
-		dockerHubTags = append(dockerHubTags, finalData)
+
+		jsonDataFromHttp, err := ioutil.ReadAll(resp.Body)
+
+		var dockerHubImage domain.DockerHubImage
+
+		err = njson.Unmarshal(jsonDataFromHttp, &dockerHubImage)
+
+		if err != nil {
+			fmt.Print(err)
+		}
+
+		for _, data := range dockerHubImage.Results {
+			var finalData domain.DockerHubTags
+			errormessage := njson.Unmarshal([]byte(data), &finalData)
+
+			if errormessage != nil {
+				fmt.Println(errormessage)
+			}
+
+			encoded, _ := json.Marshal(finalData.Tag)
+
+			rdb.RPush(ctx, image+"-"+tag, encoded)
+
+			dockerHubTags = append(dockerHubTags, string(encoded))
+		}
+		fmt.Println(rdb.LRange(ctx, image+"-"+tag, 0, -1).Val())
 	}
-
-	//data := dockerHubTags
-	//data := []string{"a", "b", "c", "d", "e"}
-	//for _, v := range data {
-	//	rdb.RPush(ctx, image+"-"+tag, []byte(v))
-	//}
-
-	test := rdb.LRange(ctx, image+"-"+tag, 0, -1)
-	fmt.Println(test)
 
 	dockerHubResult := domain.DockerHubResult{
 		Name: image,
 		Tags: dockerHubTags,
 	}
-
-	//fmt.Print(dockerHubResult)
-
 	return dockerHubResult, nil
 }
 
